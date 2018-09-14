6c9aae0908f5482b6580f9892cffd9ff8e7ee37a
fabirt <fandig27@gmail.com>
2018-09-13 19:57:50 -0500

    Adding visualizer

---
+++ RegimUI/Regim/DVisual.py
@@ -0,0 +1,137 @@
+#  -*- coding: utf-8 -*-
+
+import cv2
+import numpy
+from PIL import Image, ImageEnhance
+
+try:
+    from Tkinter import *
+except ImportError:
+    from tkinter import *
+
+# path = "C:/Users/Fabian/Desktop/Fabi_py_Projects/projects/Data_analysis/Data/Input/K/input_1.png"
+# original_image = cv2.imread(path, 0)
+# original_height, original_width = original_image.shape[:2]
+# factor = 3
+# new_width = int(original_width * factor)
+# new_height = int(original_height * factor)
+# resized_image = cv2.resize(original_image, (new_width, new_height))
+# a = Image.fromarray(resized_image).show()
+
+
+class DVisual:
+    def __init__(self, top=None):
+        _side_bg_color = '#535353'
+        _main_bg_color = '#282828'
+        _fg_color = '#000000'
+
+        _font9 = "-family Verdana -size 9 -weight normal -slant roman" \
+                " -underline 0 -overstrike 0"
+        _font11 = "-family Verdana -size 11 -weight normal -slant roman" \
+                 " -underline 0 -overstrike 0"
+        _font13 = "-family Verdana -size 13 -weight normal -slant roman " \
+                 "-underline 0 -overstrike 0"
+
+        # Creating all the GUI
+        screen_width = int(top.winfo_screenwidth() * 0.85)
+        screen_height = int(top.winfo_screenheight() - 76)
+        padx = int((top.winfo_screenwidth() / 2) - (screen_width / 2))
+        screen_size = "{0}x{1}+{2}+0".format(screen_width, screen_height, padx)
+        top.geometry(screen_size)
+        top.title("DVisual")
+        # top.iconbitmap(self.icon_path)
+        top.resizable(False, False)
+        top.configure(background="#d9d9d9")
+
+        # Frames configuration
+        self.frame_images = Frame(top)
+        self.frame_images.place(relx=0.0, rely=0.0, height=screen_height, width=screen_width*0.2)
+        self.frame_images.configure(relief=SUNKEN)
+        self.frame_images.configure(borderwidth="1")
+        self.frame_images.configure(background=_side_bg_color)
+
+        self.frame_visual = Frame(top)
+        self.frame_visual.place(relx=0.2, rely=0.0, height=screen_height, width=screen_width * 0.65)
+        self.frame_visual.configure(relief=SUNKEN)
+        self.frame_visual.configure(borderwidth="1")
+        self.frame_visual.configure(background=_main_bg_color)
+
+        self.frame_sliders = Frame(top)
+        self.frame_sliders.place(relx=0.85, rely=0.0, height=screen_height, width=screen_width * 0.15)
+        self.frame_sliders.configure(relief=SUNKEN)
+        self.frame_sliders.configure(borderwidth="1")
+        self.frame_sliders.configure(background=_side_bg_color)
+
+        # Thumbnail frames configuration
+        Tk.update(top)
+        self.tn_width = int(self.frame_images.winfo_width()) * 0.6
+
+        self.frame_fixed = Frame(self.frame_images)
+        self.frame_fixed.place(relx=0.19, rely=0.04, height=self.tn_width, width=self.tn_width)
+        self.frame_fixed.configure(relief=SUNKEN)
+        self.frame_fixed.configure(borderwidth="1")
+        self.frame_fixed.configure(background=_main_bg_color)
+        self.frame_fixed.configure(cursor="hand2")
+
+        self.frame_moving = Frame(self.frame_images)
+        self.frame_moving.place(relx=0.19, rely=0.28, height=self.tn_width, width=self.tn_width)
+        self.frame_moving.configure(relief=SUNKEN)
+        self.frame_moving.configure(borderwidth="1")
+        self.frame_moving.configure(background=_main_bg_color)
+        self.frame_moving.configure(cursor="hand2")
+
+        self.frame_reg = Frame(self.frame_images)
+        self.frame_reg.place(relx=0.19, rely=0.52, height=self.tn_width, width=self.tn_width)
+        self.frame_reg.configure(relief=SUNKEN)
+        self.frame_reg.configure(borderwidth="1")
+        self.frame_reg.configure(background=_main_bg_color)
+        self.frame_reg.configure(cursor="hand2")
+
+        self.frame_bw = Frame(self.frame_images)
+        self.frame_bw.place(relx=0.19, rely=0.76, height=self.tn_width, width=self.tn_width)
+        self.frame_bw.configure(relief=SUNKEN)
+        self.frame_bw.configure(borderwidth="1")
+        self.frame_bw.configure(background=_main_bg_color)
+        self.frame_bw.configure(cursor="hand2")
+
+        # Main visualizer frame configuration
+        Tk.update(top)
+        visual_width = int(self.frame_visual.winfo_width())
+        visual_height = int(self.frame_visual.winfo_height())
+        if visual_width < visual_height:
+            self.visual_size = visual_width
+        else:
+            self.visual_size = visual_height
+        padx_visual = int((visual_width-self.visual_size) / 2)
+        self.frame_visual_inner = Frame(self.frame_visual)
+        self.frame_visual_inner.place(x=padx_visual, rely=0.0, height=self.visual_size, width=self.visual_size)
+        self.frame_visual_inner.configure(relief=SUNKEN)
+        self.frame_visual_inner.configure(borderwidth="0")
+        self.frame_visual_inner.configure(background="#000")
+        self.frame_visual_inner.configure(cursor="fleur")
+
+        # Sliders configuration
+        # Brightness slider
+        self.scale_br = Scale(self.frame_sliders, from_=0, to=4, orient=HORIZONTAL, resolution=0.2)
+        self.scale_br.place(relx=0.0, rely=0.05, relwidth=1)
+        self.scale_br.configure(background=_side_bg_color)
+        self.scale_br.configure(activebackground="#202020")
+        self.scale_br.configure(foreground="#fff")
+        self.scale_br.configure(borderwidth="0")
+        self.scale_br.configure(command="")
+        self.scale_br.set(1)
+        # Contrast slider
+        self.scale_ct = Scale(self.frame_sliders, from_=0, to=4, orient=HORIZONTAL, resolution=0.2)
+        self.scale_ct.place(relx=0.0, rely=0.15, relwidth=1)
+        self.scale_ct.configure(background=_side_bg_color)
+        self.scale_ct.configure(activebackground="#202020")
+        self.scale_ct.configure(foreground="#fff")
+        self.scale_ct.configure(borderwidth="0")
+        self.scale_ct.configure(command="")
+        self.scale_ct.set(1)
+
+
+if __name__ == '__main__':
+    root = Tk()
+    DVisual(root)
+    root.mainloop()
