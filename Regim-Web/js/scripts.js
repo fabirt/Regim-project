3beb29a67fbeb8c91413a710cd8218781622b201
fabirt <fandig27@gmail.com>
2018-09-19 12:41:22 -0500

    Update

---
+++ Regim-Web/js/scripts.js
@@ -0,0 +1,18 @@
+function w3_bar_menu() {
+    const bars = "fa fa-bars";
+    const times = "fa fa-times";
+    const menuIcon = document.getElementById("menu-icon");
+    const iconClassName = menuIcon.className;
+    if (iconClassName==bars){
+        document.getElementById("mySidebar").style.width = "400px";
+        document.getElementById("mySidebar").style.display = "block";
+        document.getElementById("myOverlay").style.display = "block";
+        document.getElementById("menu-icon").className = times;
+    }else{
+        document.getElementById("mySidebar").style.display = "none";
+        document.getElementById("myOverlay").style.display = "none";
+        document.getElementById("menu-icon").className = bars;
+    }
+    
+    
+}
